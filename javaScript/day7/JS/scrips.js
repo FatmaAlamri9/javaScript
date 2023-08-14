3ae1c2c4291a9e7d581c5b4783a2361f24bcda20
FatmaAlamri9 <141577087+FatmaAlamri9@users.noreply.github.com>
2023-08-14 23:39:19 +0400

    Add files via upload

---
+++ javaScript/day7/JS/scrips.js
@@ -0,0 +1,49 @@
+// let text =document.getElementsByClassName('x');
+// let x = "<h1>fatma</h1>"
+// x.innerHtml = text;
+// text.innerHtml = "<h2>hamed</h2>";
+// console.log(text)
+
+// let  btn = document.getElementById('btn')
+// btn.addEventListener('click',function(){
+//     let inp1 = docId("inp1");
+//     let inp2 = docId("inp2");
+//     let text = docId('text');
+//     let output = inp1.value+ ""+inp2.value;
+//     text.innerHTML = output
+// //console.log(output)
+// })
+
+//--------------
+// let btn = document.getElementById('btn')
+// btn.addEventListener('click',function(){
+//     let box = document.getElementsByClassName('box')[0]
+//     //  let x = document.getElementsByClassName('x')[1]
+//     // box.classList.toggle('x')
+
+// })
+//--------------
+
+// let box = document.getElementsByClassName('box')
+// box.addEventListener('keydown',function(){
+//     let box = document.getElementsByClassName('box')[0]
+//     console.log(letter.code)
+//    if(letter.code==="keyArowRight")
+//    {
+//     console.log(box.move.right);
+    
+//    }
+//    else if(letter.code==="keyArowleft")
+//    {
+//     console.log(letter.code);
+    
+//    }
+    
+// })
+
+//-----------
+setTimeout(function(){
+   
+    console.log("hello")
+   
+},  3000)  
