9b69fa768ec4216eb51a9a017e6a746a7f3470ea
FatmaAlamri9 <141577087+FatmaAlamri9@users.noreply.github.com>
2023-08-22 11:28:13 +0400

    Add files via upload

---
+++ javaScript/day11/script.js
@@ -0,0 +1,34 @@
+//change the img every 3 seconds: (work)
+
+// let sources =['img/jupiter.png' , 'img/mars.png' ,'img/planet-earth.png']
+// let img = document.querySelector('img')
+// let x =0;
+
+// setInterval(() =>{
+//      x++
+//       img.setAttribute("src",sources[x]);
+//    if(x>sources.length)
+//    {
+//    x=0
+  
+//    }
+// }, 3000);
+
+// -----------------------------------
+
+//change the color by button or swaip
+
+// let main = document.querySelector('main')
+// let items = document.getElementsByClassName('items')
+
+// for(let i=0;i<items.length;i++)
+// {
+//     items[i].addEventListener('click',function(){
+//         if(i==0){
+//             //main.
+//         }
+//     })
+// }
+//--------------------------------------------
+
+
